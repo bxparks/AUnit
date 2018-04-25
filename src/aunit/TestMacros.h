25dd67da23906e368124817b1f2b062b49fc6958
Brian Park <brian@xparks.net>
2018-04-25 12:33:53 -0700

    Remove support for F() macro for various test*() macros for ESP8266 because they break any user-supplied code which uses F() in an inline context

--- src/aunit/TestMacros.h
+++ src/aunit/TestMacros.h
@@ -28,8 +28,9 @@ SOFTWARE.
 /**
  * @file TestMacros.h
  *
- * Various macros (test(), testing(), externTest(), externTesting()) are
- * defined in this header.
+ * Various macros (test(), testF(), testing(), testingF(), externTest(),
+ * externTestF(), externTesting(), externTestingF()) are defined in this
+ * header.
  */
 
 #ifndef AUNIT_TEST_MACROS_H
@@ -37,16 +38,19 @@ SOFTWARE.
 
 #include <stdint.h>
 #include <Arduino.h> // F() macro
-#include "Flash.h"
+#include "Flash.h" // AUNIT_F() macro
 #include "FCString.h"
 #include "TestOnce.h"
 #include "TestAgain.h"
 
-// On the ESP8266 platform, The F() string cannot be placed in an inline
+// On the ESP8266 platform, the F() string cannot be placed in an inline
 // context, because it interferes with other PROGMEM strings. See
 // https://github.com/esp8266/Arduino/issues/3369. The solution was to move the
 // constructor definition out from an inline function into a normal function
-// defined outside of the class declaration..
+// defined outside of the class declaration. Unfortunately, if the user code
+// has any other usage of F() in an inline context, those interfere with the
+// F() used below. I have abandoned supporting the F() macro for these test*()
+// macros on the ESP8266.
 
 /** Macro to define a test that will be run only once. */
 #define test(name) struct test_ ## name : aunit::TestOnce {\
@@ -54,7 +58,7 @@ SOFTWARE.
   virtual void once() override;\
 } test_ ## name ## _instance;\
 test_ ## name :: test_ ## name() {\
-  init(F(#name)); \
+  init(AUNIT_F(#name)); \
 }\
 void test_ ## name :: once()
 
@@ -68,7 +72,7 @@ void test_ ## name :: once()
   virtual void again() override;\
 } test_ ## name ## _instance;\
 test_ ## name :: test_ ## name() {\
-  init(F(#name));\
+  init(AUNIT_F(#name));\
 }\
 void test_ ## name :: again()
 
@@ -106,7 +110,7 @@ struct testClass ## _ ## name : testClass {\
   virtual void once() override;\
 } testClass ## _ ## name ## _instance;\
 testClass ## _ ## name :: testClass ## _ ## name() {\
-  init(F(#testClass "_" #name));\
+  init(AUNIT_F(#testClass "_" #name));\
 }\
 void testClass ## _ ## name :: once()
 
@@ -121,7 +125,7 @@ struct testClass ## _ ## name : testClass {\
   virtual void again() override;\
 } testClass ## _ ## name ## _instance;\
 testClass ## _ ## name :: testClass ## _ ## name() {\
-  init(F(#testClass "_" #name));\
+  init(AUNIT_F(#testClass "_" #name));\
 }\
 void testClass ## _ ## name :: again()
 
