f813e81bb8100ba96811b0c794ced70026755fe4
Brian Park <brian@xparks.net>
2018-06-19 13:35:43 -0700

    Initial upload of build_arduino.sh and serial_monitor.py scripts to GitHub

---
+++ tools/build_arduino.sh
@@ -0,0 +1,326 @@
+#!/usr/bin/env bash
+#
+# A shell wrapper around the arduino(1) program with the following
+# additional features:
+#
+#   1) Supports 3 modes: verify (--verify), upload (--upload), and test
+#      (--test).
+#   2) Multiple *.ino files can be given, and the arduino(1) binary will
+#      be executed for each sketch.
+#   3) A directory can be given and the script will infer the corresponding
+#      *.ino file under that directory.
+#   4) Board aliases can be defined in a user-defined dotfile (e.g.
+#      ~/.build_arduino_config) which maps a short alias (e.g. "nano") to the
+#      longer board spec used by the arduino binary (e.g.
+#      "arduino:avr:nano:cpu=atmega328old").
+#   5) The script can monitor the serial output of the board immediately after
+#      uploading the sketch using the 'serial_monitor.py' helper script.
+#   6) If the sketch is a unit test written in AUnit, the 'serial_monitor.py'
+#      can be told to determine if the unit test passes or fails, and result
+#      will be returned to the shell script, which can collect and print
+#      a summary of all unit tests that was invoked by the script.
+#   7) If multiple board/port pairs are given using the --board flag, then
+#      the entire set of *.ino files are run through the arduino binary for each
+#      board/port pair. This is useful for running a set of unit tests for
+#      multiple board types at the same time.
+#
+# Usage:
+#
+#   $ build_arduino.sh [--help] [--verbose] [--verify | --upload | --test ]
+#       [--monitor] [--port /dev/ttyUSB0] [--baud baud]
+#       [--board {package}:{arch}:{board}[:parameters]]
+#       [--boards {alias}:{port},...] (file.ino | dir) [...]
+#
+# Documentation of arduino binary flags:
+#
+#   https://github.com/arduino/Arduino/blob/master/build/shared/manpage.adoc
+#
+# Environment variables:
+#
+#   BUILD_ARDUINO_BINARY: location of the arduino IDE binary
+#   BUILD_ARDUINO_CONFIG: location of the .build_arduino_config file
+#
+# Example ports (Linux):
+#
+#   - Arduino Micro: /dev/ttyACM{n}
+#   - Arduino Nano: /dev/ttyUSB{n}
+#   - ESP8266: /dev/ttyUSB{n}
+#
+# Example .build_arduino_config alias file:
+#
+#   uno=arduino:avr:uno
+#   nano=arduino:avr:nano:cpu=atmega328old
+#   leonardo=arduino:avr:leonardo
+#   esp8266=esp8266:esp8266:nodemcuv2
+#
+# Flags:
+#
+#   If the directory is given, then the script looks for a sketch file under
+#   the directory with the same name but ending with '.ino'. For example,
+#   './build_arduino.sh CommonTest' is equivalent to './build_arduino.sh
+#   CommonTest/CommonTest.ino' if CommonTest is a directory.
+#
+#   The --test flag automatically enables both the --upload and the --monitor
+#   flags.
+#
+#   If none of --verify, --upload nor --test flags are given, then the default
+#   is --verify.
+#
+#   The default value of --port is /dev/ttyUSB0.
+#
+#   The default value of --baud is 115200.
+#
+# Example:
+#
+#   $ ./build_arduino.sh --port /dev/ttyUSB0
+#       --board arduino:avr:nano:cpu=atmega328old --verify Blink.ino
+#
+#   $ ./build_arduino.sh --port /dev/ttyUSB0 \
+#       --board arduino:avr:nano:cpu=atmega328old --upload CommonTest.ino
+#
+#   $ ./build_arduino.sh --port /dev/ttyUSB0 \
+#       --board arduino:avr:lenardo --test CommonTest.ino
+#
+#   $ ./build_arduino.sh --port /dev/ttyUSB0 \
+#       --board esp8266:esp8266:nodemcuv2 --test CommonTest.ino
+#
+#   $ ./build_arduino.sh --test \
+#       --boards nano:/dev/ttyUSB1,leonardo:/dev/ttyACM0 tests/*Test
+#
+# Dependencies:
+#
+#   - pyserial (tested with 3.4-1)
+#   - Arduion IDE (tested with 1.8.5)
+#
+# Copyright and License:
+#
+#   Copyright 2018 (c) Brian T. Park <brian@xparks.net>
+#   MIT License
+
+set -eu
+
+# Can't use $(realpath $(dirname $0)) because realpath doesn't exist on MacOS
+DIRNAME=$(dirname $0)
+
+function usage() {
+    echo "Usage: build_arduino.sh [--help] [--verbose] \
+[--verify | --upload | --test ] [--monitor] \
+[--port port] [--baud baud] [--board board] \
+[--boards {alias}:{port},...] (file.ino | directory) [...]"
+    exit 1
+}
+
+# Find the *.ino file, even if only the directory was given, e.g. "CommonTest"
+function get_ino_file() {
+    local file=$1
+
+    # Ends in '.ino', just return it.
+    if [[ "$file" =~ .*\.ino ]]; then
+        echo $file
+        return
+    fi
+
+    # Not a directory, don't know what to do with it, just return it
+    if [[ ! -d $file ]]; then
+        echo $file
+        return
+    fi
+
+    # Strip off any trailing '/'
+    local dir=$(echo $file | sed -e 's/\/*$//')
+    local file=$(basename $dir)
+    echo "${dir}/${file}.ino"
+}
+
+function get_config() {
+    local key=$(echo $1 | sed -e 's/\./\\./g')
+    local matching_line=$(grep "^$key=" $BUILD_ARDUINO_CONFIG)
+    if [[ $matching_line != '' ]]; then
+        local value=$(echo $matching_line | sed -e "s/^$key=\(.*\)/\1/")
+        echo $value
+    else
+        echo ''
+    fi
+}
+
+function run_arduino_command_for_boards() {
+    local board_envs=$(echo $boards | sed -e 's/,/ /')
+    for env in $board_envs; do
+        # Split {alias}:{port} into two fields.
+        local board_alias=$(echo $env | sed -e 's/\([^:]*\):\([^:]*\)/\1/')
+        local board_port=$(echo $env | sed -e 's/\([^:]*\):\([^:]*\)/\2/')
+
+        echo "======== Processing board=$board_alias, port=$board_port"
+        local board_value=$(get_config $board_alias)
+        if [[ "$board_value" == '' ]]; then
+            echo "Unknown board alias '$board_alias'"
+            continue
+        fi
+
+        board=$board_value
+        port=$board_port
+        run_arduino_command_for_files "$@"
+    done
+}
+
+function run_arduino_command_for_files() {
+    local file
+    for file in "$@"; do
+        local ino_file=$(get_ino_file $file)
+        if [[ ! -f $ino_file ]]; then
+            echo "==== Cannot find $ino_file"
+            continue
+        fi
+
+        set +e; run_arduino_command $ino_file; local status=$?; set -e
+        if [[ $status != 0 ]]; then
+            continue
+        fi
+
+        # Run the serial monitor in test validation mode if --test flag given.
+        if [[ "$mode" == 'test' ]]; then
+            echo # blank line
+            local cmd="$DIRNAME/serial_monitor.py --test \
+--port $port --baud $baud"
+            echo "\$ $cmd"
+            set +e; $cmd; local status=$?; set -e
+            if [[ "$status" == 0 ]]; then
+                echo "PASSED: $board $port $ino_file" >> $test_summary_file
+            else
+                echo "FAILED: $board $port $ino_file" >> $test_summary_file
+            fi
+        # Just run the serial monitor in echo mode.
+        elif [[ "$monitor" == 1 ]]; then
+            echo # blank line
+            local cmd="$DIRNAME/serial_monitor.py --port $port --baud $baud"
+            echo "\$ $cmd"
+            set +e; $cmd; local status=$?; set -e
+        fi
+    done
+}
+
+function run_arduino_command() {
+    local file=$1
+    echo "==== Processing $file"
+
+    if [[ "$board" == '' ]]; then
+        echo 'Board variable $board not defined, should not happen'
+        return 1
+    fi
+    if [[ "$port" == '' ]]; then
+        echo 'Port variable $port not defined, should not happen'
+        return 1
+    fi
+
+    local board_flag="--board $board"
+    local upload_or_verify='--verify'
+    if [[ "$mode" == 'upload' || "$mode" == 'test' ]]; then
+        upload_or_verify='--upload'
+    fi
+
+    # Execute the arduino(1) command line and handle the error
+    local cmd="$BUILD_ARDUINO_BINARY $verbose $upload_or_verify --port $port \
+$board_flag $file"
+    echo "\$ $cmd"
+    set +e; $cmd; local status=$?; set -e
+    if [[ "$verbose" != '' && "$mode" == 'test' && $status == 0 ]]; then
+        echo # blank line
+    fi
+    if [[ "$status" != 0 ]]; then
+        echo "Failed processing: $file"
+    fi
+    return $status
+}
+
+function clean_test_summary_file() {
+    if [[ "$test_summary_file" != '' ]]; then
+        rm -f $test_summary_file
+    fi
+}
+
+function create_test_summary_file() {
+    test_summary_file=$(mktemp /tmp/build_arduino.XXXXXX.txt)
+    trap "clean_test_summary_file" EXIT
+}
+
+function print_test_summary_file() {
+    echo '======== Test Run Summary'
+    cat $test_summary_file
+    set +e; grep --quiet FAILED $test_summary_file; local status=$?; set -e
+    if [[ "$status" != 0 ]]; then
+        echo 'ALL PASSED'
+    else
+        echo 'FAILURES'
+    fi
+}
+
+function check_environment_variables() {
+    # Check for BUILD_ARDUINO_BINARY
+    if [[ -z ${BUILD_ARDUINO_BINARY+x} ]]; then
+        echo "BUILD_ARDUINO_BINARY environment variable is not defined"
+        exit 1
+    fi
+    if [[ ! -x $BUILD_ARDUINO_BINARY ]]; then
+        echo "BUILD_ARDUINO_BINARY=$BUILD_ARDUINO_BINARY is not an executable"
+        exit 1
+    fi
+
+    # Check for BUILD_ARDUINO_CONFIG
+    if [[ -z ${BUILD_ARDUINO_CONFIG+x} ]]; then
+        echo "BUILD_ARDUINO_CONFIG environment variable is not defined"
+        exit 1
+    fi
+    if [[ ! -f $BUILD_ARDUINO_CONFIG ]]; then
+        echo "BUILD_ARDUINO_CONFIG=$BUILD_ARDUINO_CONFIG is not readable"
+        exit 1
+    fi
+}
+
+check_environment_variables
+
+# Parse command line flags
+port=/dev/ttyUSB0
+board=
+boards=
+mode='--upload'
+monitor=0
+baud=115200
+verbose=
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --verify) mode='verify' ;;
+        --upload) mode='upload' ;;
+        --test) mode='test' ;;
+        --port) shift; port=$1 ;;
+        --baud) shift; baud=$1 ;;
+        --board) shift; board=$1 ;;
+        --boards) shift; boards=$1 ;;
+        --monitor) monitor=1 ;;
+        --verbose) verbose='--verbose' ;;
+        --help|-h) usage ;;
+        -*) echo "Unknown option '$1'"; usage ;;
+        *) break ;;
+    esac
+    shift
+done
+if [[ "$port" == '' ]]; then
+    echo '--port flag must be given'
+    usage
+fi
+
+# Set up temporary summary file
+if [[ "$mode" == 'test' ]]; then
+    create_test_summary_file
+fi
+
+# Run for various boards and/or files
+if [[ "$boards" != '' ]]; then
+    run_arduino_command_for_boards "$@"
+else
+    run_arduino_command_for_files "$@"
+fi
+
+# Print test run summary
+if [[ "$mode" == 'test' ]]; then
+    print_test_summary_file
+fi
