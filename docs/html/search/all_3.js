f8e2e44efdf377d3dee75ddeb29ddc9ae9c76b2e
Brian Park <brian@xparks.net>
2018-12-01 14:46:16 -0800

    docs: update doxygen docs for v1.2

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -4,8 +4,8 @@ var searchData=
   ['exclude',['exclude',['../classaunit_1_1TestRunner.html#ad55fbb77f52eb2c1a2d45536e8c4afba',1,'aunit::TestRunner::exclude(const char *pattern)'],['../classaunit_1_1TestRunner.html#aacd40834554b476893701d7492d2d550',1,'aunit::TestRunner::exclude(const char *testClass, const char *pattern)']]],
   ['expire',['expire',['../classaunit_1_1Test.html#aab89c47bfa768b0dbf9eb18a777ac4bc',1,'aunit::Test']]],
   ['expiretestnow',['expireTestNow',['../MetaAssertMacros_8h.html#a0f865d01d2abffc7ba9365ebcdb3eda9',1,'MetaAssertMacros.h']]],
-  ['externtest',['externTest',['../TestMacros_8h.html#a5aec0442c0e47ce1c485be5baffa9c36',1,'TestMacros.h']]],
+  ['externtest',['externTest',['../TestMacros_8h.html#a144a141e11afa7d1071965e962dbfb42',1,'TestMacros.h']]],
   ['externtestf',['externTestF',['../TestMacros_8h.html#a411a4acc9c28fd3d2e9e83b63b91e145',1,'TestMacros.h']]],
-  ['externtesting',['externTesting',['../TestMacros_8h.html#ae3759a8b15c8390bf29e68da59161440',1,'TestMacros.h']]],
+  ['externtesting',['externTesting',['../TestMacros_8h.html#a6f5d7f585ea78b8e7bee81bc655de07e',1,'TestMacros.h']]],
   ['externtestingf',['externTestingF',['../TestMacros_8h.html#a00e313531a1972e4ad46c4681b52512a',1,'TestMacros.h']]]
 ];
