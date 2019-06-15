306528d706e1ae6a5853ef1f56a72761ef5b3b30
Brian Park <brian@xparks.net>
2019-06-15 15:45:26 -0700

    docs: Regenerate doxygen docs

--- docs/html/search/all_5.js
+++ docs/html/search/all_5.js
@@ -3,13 +3,13 @@ var searchData=
   ['getbuffer',['getBuffer',['../classaunit_1_1fake_1_1FakePrint.html#ab3a88258aa921031badaed5f24a676e5',1,'aunit::fake::FakePrint']]],
   ['getcstring',['getCString',['../classaunit_1_1internal_1_1FCString.html#a03e7eb782104ca65cb2dfe161540833e',1,'aunit::internal::FCString']]],
   ['getfstring',['getFString',['../classaunit_1_1internal_1_1FCString.html#a4139be1f5381faebcb54b3d357957cb4',1,'aunit::internal::FCString']]],
-  ['getlifecycle',['getLifeCycle',['../classaunit_1_1Test.html#a1796d740730201292f84e40b58167be2',1,'aunit::Test']]],
+  ['getlifecycle',['getLifeCycle',['../classaunit_1_1Test.html#a451274cf4d90ca3a626cdb0781b71685',1,'aunit::Test']]],
   ['getname',['getName',['../classaunit_1_1Test.html#afc5f564a39de7fd5cef0819767656ab2',1,'aunit::Test']]],
   ['getnext',['getNext',['../classaunit_1_1Test.html#ac166f92c4945d675b4e289db1bb7d217',1,'aunit::Test']]],
   ['getprinter',['getPrinter',['../classaunit_1_1Printer.html#ae3783da78df10b7abff74826904ce5c4',1,'aunit::Printer']]],
   ['getroot',['getRoot',['../classaunit_1_1Test.html#a33f9f14097b77edc19e8298022ecbe60',1,'aunit::Test']]],
-  ['getstatus',['getStatus',['../classaunit_1_1Test.html#a30c8ffa6526ea3efa1e552e261774716',1,'aunit::Test']]],
+  ['getstatus',['getStatus',['../classaunit_1_1Test.html#aa709615a0c842dbd4df47744143eef5c',1,'aunit::Test']]],
   ['gettype',['getType',['../classaunit_1_1internal_1_1FCString.html#a18d41c990f2843ac1f922d4ca5c65399',1,'aunit::internal::FCString']]],
-  ['getverbosity',['getVerbosity',['../classaunit_1_1Test.html#a92a847829527808fd09bd9a526b57ad3',1,'aunit::Test']]],
+  ['getverbosity',['getVerbosity',['../classaunit_1_1Test.html#a86b8d967069abc8652ffa2f3d4ce8a5f',1,'aunit::Test']]],
   ['gtest_2eh',['gtest.h',['../gtest_8h.html',1,'']]]
 ];
--- docs/html/search/functions_6.js
+++ docs/html/search/functions_6.js
@@ -1,17 +1,17 @@
 var searchData=
 [
   ['include',['include',['../classaunit_1_1TestRunner.html#a63301f8ab1cbbf1b7cca7a35434b00d2',1,'aunit::TestRunner::include(const char *pattern)'],['../classaunit_1_1TestRunner.html#a8aca88d9605b34e07cca54c6ab99d6b1',1,'aunit::TestRunner::include(const char *testClass, const char *pattern)']]],
-  ['isdone',['isDone',['../classaunit_1_1Test.html#a7e02b5484eae65788f738e50aa44699f',1,'aunit::Test']]],
-  ['isexpired',['isExpired',['../classaunit_1_1Test.html#af3e8f6851777d79ac71ebfb7710ffcaf',1,'aunit::Test']]],
-  ['isfailed',['isFailed',['../classaunit_1_1Test.html#a56108e7adeb547cc51dbe3ca2343194f',1,'aunit::Test']]],
-  ['isnotdone',['isNotDone',['../classaunit_1_1Test.html#abf2f92ad1680e1c8fcceaedc54172228',1,'aunit::Test']]],
-  ['isnotexpired',['isNotExpired',['../classaunit_1_1Test.html#aa1eb8aa9f1527a2e9d3bdd863f894309',1,'aunit::Test']]],
-  ['isnotfailed',['isNotFailed',['../classaunit_1_1Test.html#a57edd2dabfa7f6b2c91a78dad1637370',1,'aunit::Test']]],
-  ['isnotpassed',['isNotPassed',['../classaunit_1_1Test.html#a923f621dda088a4e47258960ec09f1da',1,'aunit::Test']]],
-  ['isnotskipped',['isNotSkipped',['../classaunit_1_1Test.html#aa7d205159104b7f96428a31eac15ca64',1,'aunit::Test']]],
-  ['isoutputenabled',['isOutputEnabled',['../classaunit_1_1Assertion.html#ac56577e807b88987f06605c104d50451',1,'aunit::Assertion']]],
-  ['isoutputenabledforstatus',['isOutputEnabledForStatus',['../classaunit_1_1MetaAssertion.html#a071edb371b360f58a65643cf9bc17781',1,'aunit::MetaAssertion']]],
-  ['ispassed',['isPassed',['../classaunit_1_1Test.html#a7a6c48fbb202d8aa6a13774487f97cad',1,'aunit::Test']]],
-  ['isskipped',['isSkipped',['../classaunit_1_1Test.html#a59cffb7aefe39adbb8b7f14cb22ac02f',1,'aunit::Test']]],
-  ['isverbosity',['isVerbosity',['../classaunit_1_1Test.html#acad6ad04fc4bf8f1068fdb5ecda70b79',1,'aunit::Test::isVerbosity()'],['../classaunit_1_1TestRunner.html#a6e9df4eb9d16fe3b56afd17603e45baa',1,'aunit::TestRunner::isVerbosity()']]]
+  ['isdone',['isDone',['../classaunit_1_1Test.html#a07a87e1169ab443429962bc55dde88cc',1,'aunit::Test']]],
+  ['isexpired',['isExpired',['../classaunit_1_1Test.html#af8f56d244a5a795bb7e1eb83844a0302',1,'aunit::Test']]],
+  ['isfailed',['isFailed',['../classaunit_1_1Test.html#a5ca38d5223dfcc81f3e8c7a0917c383d',1,'aunit::Test']]],
+  ['isnotdone',['isNotDone',['../classaunit_1_1Test.html#a05359103570aad48e4efdb7f3bc4416f',1,'aunit::Test']]],
+  ['isnotexpired',['isNotExpired',['../classaunit_1_1Test.html#a4b58ed5f8cc54608d0a35a255aa27d97',1,'aunit::Test']]],
+  ['isnotfailed',['isNotFailed',['../classaunit_1_1Test.html#a3976d88474bb9a7b75b16d9458d9a9cb',1,'aunit::Test']]],
+  ['isnotpassed',['isNotPassed',['../classaunit_1_1Test.html#a1797e5f99e8ed21d886f77ab9da8289f',1,'aunit::Test']]],
+  ['isnotskipped',['isNotSkipped',['../classaunit_1_1Test.html#a9d5368f632ee74592813afc6f19ca46b',1,'aunit::Test']]],
+  ['isoutputenabled',['isOutputEnabled',['../classaunit_1_1Assertion.html#ab8f2303b22e6f1380575c48809052e77',1,'aunit::Assertion']]],
+  ['isoutputenabledforstatus',['isOutputEnabledForStatus',['../classaunit_1_1MetaAssertion.html#af3daad8e882c15d94884f22609b32777',1,'aunit::MetaAssertion']]],
+  ['ispassed',['isPassed',['../classaunit_1_1Test.html#a250e09283ea4304cedaa9cc0029ee026',1,'aunit::Test']]],
+  ['isskipped',['isSkipped',['../classaunit_1_1Test.html#a64a8e33b6498b0c2605c43472b6cbec6',1,'aunit::Test']]],
+  ['isverbosity',['isVerbosity',['../classaunit_1_1Test.html#a48965aa2e166e680664ccd5b5109f4a3',1,'aunit::Test::isVerbosity()'],['../classaunit_1_1TestRunner.html#a6e9df4eb9d16fe3b56afd17603e45baa',1,'aunit::TestRunner::isVerbosity()']]]
 ];
