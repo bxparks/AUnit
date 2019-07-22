6732129b6928a979810ffead31cec80ff8167fa0
Brian Park <brian@xparks.net>
2019-07-22 14:23:27 -0700

    src: Use SerialUSB instead of Serial for the Arduino Zero and other SAMD21 M0 clones

--- src/aunit/Printer.cpp
+++ src/aunit/Printer.cpp
@@ -25,8 +25,18 @@ SOFTWARE.
 #include <Arduino.h> // Serial
 #include "Printer.h"
 
+// All other SAMD boards define SERIAL_PORT_MONITOR as SerialUSB, except for
+// the Zero, which defines it to be Serial, which doesn't work. Clobber it.
+#if defined(ARDUINO_SAMD_ZERO)
+  #undef SERIAL_PORT_MONITOR
+  #define SERIAL_PORT_MONITOR SerialUSB
+#elif defined(__linux__) or defined(__APPLE__)
+  #undef SERIAL_PORT_MONITOR
+  #define SERIAL_PORT_MONITOR Serial
+#endif
+
 namespace aunit {
 
-Print* Printer::sPrinter = &Serial;
+Print* Printer::sPrinter = &SERIAL_PORT_MONITOR;
 
 }
